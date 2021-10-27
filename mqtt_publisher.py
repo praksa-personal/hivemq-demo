06a4c7336c768ea4abe22142ec6e5a0daf6ff943
marko874 <marko874@gmail.com>
2021-10-27 20:43:13 +0200

    Hive mq demo working with broker on localhost

--- mqtt_publisher.py
+++ mqtt_publisher.py
@@ -1,33 +1,18 @@
-import paho.mqtt.client as mqtt
+import paho.mqtt.client as paho
+import time
 
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        print("Connected successfully")
-    else:
-        print("Connect returned result code: " + str(rc))
+def on_publish(client, userdata, mid):
+    print("mid: "+str(mid))
+ 
 
-# create the client
-client = mqtt.Client()
-client.on_connect = on_connect
+client = paho.Client(client_id="Publisher", clean_session=True, userdata=None, protocol=paho.MQTTv31)
+client.on_publish = on_publish
+client.connect(host="localhost", port=1883)
+client.loop_start()
 
-# enable TLS
-client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS)
+data = 0
 
-# set username and password
-client.username_pw_set("NewUser", "NewUser1234")
-
-# connect to HiveMQ Cloud on port 8883
-client.connect("874b51c1e4f04dc9838ed3d9289a3600.s1.eu.hivemq.cloud", 8883)
-
-# subscribe to the topic "my/test/topic"
-client.subscribe("my/test/topic")
-
-# publish "Hello" to the topic "my/test/topic"
-client.publish("my/test/topic", "Hello1")
-client.publish("my/test/topic", "Hello2")
-client.publish("my/test/topic", "Hello3")
-client.publish("my/test/topic", "Hello4")
-
-# Blocking call that processes network traffic, dispatches callbacks and handles reconnecting.
-client.loop_forever()
+while True:
+    data += 1
+    (rc, mid) = client.publish("topic_1", str(data), qos=1)
+    time.sleep(5)
